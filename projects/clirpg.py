f5f3fa14d1aade816c6de2238051e15e558c761e
Tabitha <tabitha.r.mccracken@gmail.com>
2022-04-23 18:07:54 -0600

    update to cliprg

--- projects/clirpg.py
+++ projects/clirpg.py
@@ -11,18 +11,19 @@
 # If they don't have the sword, then they will be eaten by the dragon and lose the game.
 
 import requests
-player_name = str(input("Please enter your name: \n"))
 
-name_len = len(player_name)
+name_len = 0
+while not (name_len >=2 and name_len <=40):
+    player_name = str(input("Please enter your name: \n"))
+    name_len = len(player_name)
 
 URL = f"https://uzby.com/api.php?min={name_len}&max={name_len}"
 
 response = requests.get(URL)
 new_name = response.text
 
-print(f"In this game, your new name is: {new_name}")
-print(
-    f"Welcome, {new_name}! Enjoy the game, we hope you make it out alive! \n")
+print(f"Welcome, {new_name}! In this game, your new name will be: {new_name}")
+print(" Enjoy the game, we hope you make it out alive! \n")
 
 
 end_game = False
